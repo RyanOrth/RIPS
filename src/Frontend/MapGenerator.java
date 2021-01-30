b90c0e4ae4eb758be62316dd229f5b033ea474ca
morganlarson <morganlarson@10.69.46.146>
2021-01-30 13:33:27 -0600

    Added two final variables mapLength and mapHeight to MapGenerator.

---
+++ src/Frontend/MapGenerator.java
@@ -0,0 +1,7 @@
+package Frontend;
+
+public class MapGenerator {
+	private final int mapLength = 0; //choose a viable length and height for the borders of the map
+	private final int mapHeight = 0;
+	
+}
