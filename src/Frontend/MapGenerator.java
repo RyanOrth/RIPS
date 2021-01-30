b450a35e539ff7c55fdc3c77d1134c638b4e239e
morganlarson <morganlarson@10.69.46.146>
2021-01-30 15:43:46 -0600

    Added a constructor and paint graphics to display a singular line.

--- src/Frontend/MapGenerator.java
+++ src/Frontend/MapGenerator.java
@@ -1,15 +1,28 @@
 package Frontend;
 
-import java.awt.Dimension;
-
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import javax.swing.JPanel;
 import javax.swing.JFrame;
 
 public class MapGenerator extends JFrame {
-	private static JFrame pane = new JFrame();
+	
+	public MapGenerator() {
+		JPanel panel = new JPanel();
+	    getContentPane().add(panel);
+	    setSize(550, 700);
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+	    Graphics2D g2 = (Graphics2D) g;
+	    Line2D line = new Line2D.Double(50, 650, 50, 50);
+	    g2.draw(line);
+	}
 	
 	public static void main(String args[]) {
-		pane.setPreferredSize(new Dimension(300, 400));
-		pane.pack();
-		pane.setVisible(true);
+		MapGenerator map = new MapGenerator();
+		map.setVisible(true);
 	}
 }
