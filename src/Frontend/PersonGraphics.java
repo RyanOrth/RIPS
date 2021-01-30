84117c23acd7b7919926766b6de8da94342dc6a7
Zennith Boerger <Zennith.boerger@gmail.com>
2021-01-30 17:19:03 -0600

    PersonGraphics so far

--- src/Frontend/PersonGraphics.java
+++ src/Frontend/PersonGraphics.java
@@ -1,5 +1,19 @@
 package Frontend;
 
+import java.awt.Color;
+import java.awt.Shape;
+import Backend.PersonBehavior.InfectionType;
+import Backend.PersonBehavior.SafetyMeasures;
+
 public class PersonGraphics {
+	private Shape personShape;
+	private Color personColor;
 	
+	/*public Shape generateShape(InfectionType infectionType, SafetyMeasures safetyMeasures) {
+		if(safetyMeasures == safetyMeasures.WEARING_MASK){
+			
+		}else {
+			return new Circle
+		}
+	}*/
 }
