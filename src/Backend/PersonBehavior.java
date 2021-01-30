933e121f15014c3d57ef4c648cb7604689d3b030
RyanOrth <ryan.orth@live.com>
2021-01-30 13:28:27 -0600

    Added backend package and started PersonBehavior

---
+++ src/Backend/PersonBehavior.java
@@ -0,0 +1,19 @@
+package Backend;
+
+public class PersonBehavior {
+	public enum InfectionType {
+		INFECTED, NOT_INFECTED,
+	}
+	
+	public enum SafetyMeasures{
+		NO_MASK, WEARING_MASK,
+	}
+	
+	public PersonBehavior() {
+		
+	}
+	
+	private void setBehavior() {
+		
+	}
+}
