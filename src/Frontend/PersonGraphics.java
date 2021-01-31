6c08882c69a3a653d0ee0379b3798ba430cf2294
Zennith Boerger <Zennith.boerger@gmail.com>
2021-01-30 18:23:34 -0600

    Complete PersonGraphics, might need some tweaking on shape sizes.

--- src/Frontend/PersonGraphics.java
+++ src/Frontend/PersonGraphics.java
@@ -1,7 +1,10 @@
 package Frontend;
 
 import java.awt.Color;
+import java.awt.Polygon;
 import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
 import Backend.PersonBehavior.InfectionType;
 import Backend.PersonBehavior.SafetyMeasures;
 
@@ -9,11 +12,27 @@ public class PersonGraphics {
 	private Shape personShape;
 	private Color personColor;
 	
-	/*public Shape generatePersonShape(InfectionType infectionType, SafetyMeasures safetyMeasures, double xPos, double yPos) {
-		if(safetyMeasures == safetyMeasures.WEARING_MASK){
-			
+	public Object generatePersonShape(SafetyMeasures safetyMeasures, double xPos, double yPos) {
+		if(safetyMeasures == SafetyMeasures.WEARING_MASK) {
+			return new Ellipse2D.Double(xPos, yPos, 6, 6);
 		}else {
-			return new Circle(xPos, yPos, );
+			return new Polygon(new int[] {((int) xPos-3), (int) xPos, ((int) xPos + 3)}, 
+				new int[] {((int)yPos + 3), (int)yPos, ((int)yPos-3)}, 3);
 		}
-	}*/
+	}	
+	public Color generatePersonColor(InfectionType infectionType) {
+		if(infectionType == InfectionType.INFECTED) {
+			return Color.RED;
+		}else {
+			return Color.BLUE;
+		}
+	}
+	
+	public Ellipse2D.Double generateInfectionCircle(InfectionType infectionType, double xPos, double yPos){
+		if(infectionType == InfectionType.INFECTED) {
+			return new Ellipse2D.Double(xPos, yPos, 12, 12);
+		}else {
+			return new Ellipse2D.Double(xPos, yPos, 0, 0);
+		}
+	}
 }
